7cb6bb9b73987041c62ff36bd584946907e4418f
Ingo Schommer <me@chillu.com>
2021-06-14 09:25:43 +1200

    NEW Refactor to not require sake
    There's no straightforward way to have sake run without a database connection.
    When using a "fake" HTTPApplication for CLI execution, it executes middleware which rightfully assumes a database connection (e.g. fluent is getting available languages to modify routing).
    
    Relies on https://github.com/silverstripe/silverstripe-framework/pull/9977

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace SilverStripe\GraphQLComposerPlugin;
 
 use Composer\Composer;
@@ -7,9 +6,13 @@ use Composer\Plugin\PluginInterface;
 use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\Script\Event;
 use Composer\IO\IOInterface;
+use SilverStripe\Core\CoreKernel;
+use SilverStripe\GraphQL\Schema\Exception\EmptySchemaException;
 use SilverStripe\GraphQL\Schema\Schema;
 use SilverStripe\GraphQL\Schema\SchemaBuilder;
-use SilverStripe\GraphQL\Schema\Storage\CodeGenerationStoreCreator;
+
+//require_once __DIR__ . '/../../../autoload.php';
+require_once __DIR__ . '/../../../silverstripe/framework/src/includes/constants.php';
 
 class Plugin implements PluginInterface, EventSubscriberInterface
 {
@@ -44,21 +47,30 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 
     public function generateSchema(Event $event)
     {
+        $schemas = getenv('SS_GRAPHQL_COMPOSER_BUILD_SCHEMAS');
+        // TODO Filter by schema
 
-        // vendor/bin is temorarily pushed on top of PATH, see https://getcomposer.org/doc/articles/scripts.md
-        $cmd = defined('SS_GRAPHQL_COMPOSER_CMD') ? SS_GRAPHQL_COMPOSER_CMD : 'sake dev/graphql/build';
+        // Not using sake because it creates a HTTPApplication through cli-script.php.
+        // This would trigger middlewares assuming a HTTP request execution
+        // (rather than CLI), which then connect to the database that might not be available during this build.
+        $kernel = new CoreKernel(BASE_PATH, false);
+        $kernel->boot();
 
-        // Allow disabling by null'ing the env var
-        if (!$cmd) {
-            return;
-        }
-
-        // title() and section() exist in symfony/console, but not through IOInterface
-        $this->io->write('<info>######################################################</info>');
         $this->io->write('<info>silverstripe/graphql-composer-plugin: Building schemas</info>');
-        $this->io->write('<info>######################################################</info>');
 
-        $out = shell_exec($cmd);
-        $this->io->write($out);
+        $keys = array_keys(Schema::config()->get('schemas'));
+        $keys = array_filter($keys, function ($key) {
+            return $key !== Schema::ALL;
+        });
+        foreach ($keys as $key) {
+            $builder = SchemaBuilder::singleton();
+            $schema = $builder->boot($key);
+            $this->io->write(sprintf('Building schema "%s"', $key));
+            try {
+                $builder->build($schema);
+            } catch (EmptySchemaException $e) {
+                $this->io->write('error');
+            }
+        }
     }
 }
